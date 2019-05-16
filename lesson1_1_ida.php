42ce4921e774d1f1197cf2cdf77e4e9980ffd356
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-16 18:07:43 +0900

    Create lesson1_1_ida.php

---
+++ lesson1_1_ida.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<?php
+		print"おはよう。<br>";<br>
+		print"こんにちは。<br>";<br>
+		print"こんばんは。<br>";<br>
+		 ?>
+	</body>
+</html>
