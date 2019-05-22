4ddb2591c0ced3ddd73f60e02cfcef713daa88e2
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-22 17:22:43 +0900

    職種一覧画面_view作成
    それに加えて管理美術品検索画面と管理ユーザ登録画面のform action先を変更

---
+++ m_search_material.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>管理美術品検索画面</title>
+    <link rel="stylesheet" href="m_search_material.css">
+  </head>
+  <body>
+  <!-- 製作者：井田 -->
+    <?php
+    require_once("../init.php");
+     ?>
+    <h1>美術品検索画面</h1>
+    <table>
+      <form action="m_control/m_search_material.php" method="post">
+        <tr><th>美術品名</th><th><input type="text" size="30" name ="material_name"></th></tr>
+        <tr><th>美術品名読み</th><th><input type="text" size="30" name ="material_kana"></th></tr>
+        <tr><th>作者名</th><th><input type="text" size="30" name ="author_name"></th></tr>
+        <tr><th>作者名読み</th><th><input type="text" size="30" name ="author_kana"></th></tr>
+        <tr><th>ジャンル</th>
+          <th>
+          <select name="genre_id">
+            <?php while ($row = $sth->fetch(PDO::FETCH_ASSOC)) { ?>
+              <option value="<?php ph($row["genre_id"]); ?>"><?php ph($row["genre_name"]) ?></option>
+            <?php } ?>
+          </select>
+          </th>
+        </tr>
+        <tr><th>制作年</th><th><input type="text" size="30" name ="material_year"></th></tr>
+        <input type="submit" name="検索">
+        <input type="reset" name="クリア">
+      </form>
+    </table>
+  </body>
+</html>
