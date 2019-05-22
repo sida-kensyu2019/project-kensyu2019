be16bfb7f890a6618d29e7d87d3ca1ae44ee46da
7mura <50697386+7mura@users.noreply.github.com>
2019-05-22 15:01:06 +0900

    削除完了ビュー

---
+++ lib/m_view/m_delete_exec_user.php
@@ -0,0 +1,16 @@
+<?php //村上：2019.5.22：Ver1.0 管理者削除完了ビュー?>
+<!DOCTYPE html>
+ <html>
+     <head>
+       <meta charset="UTF-8">
+       <title>削除完了</title>
+       <link rel="stylesheet" href="pass">
+       <script src=""></script>
+     </head>
+     <body>
+       <h1>削除完了</h1>
+       <p>該当の管理者を削除しました。</p>
+       <br>
+       <a href="m_select_material.php">管理者一覧画面に戻る</a>
+     </body>
+ </html>
