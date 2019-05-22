22044d734b09c0557af04fd37736dc80dd9baf86
Y-Masui <50697934+Y-Masui@users.noreply.github.com>
2019-05-22 16:06:00 +0900

    Create m_select_genre.php

---
+++ m_view/m_select_genre.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>管理トップページ</title>
+    <link rel="stylesheet" href="m_top.css">
+</head>
+<body>
+  <h1>美術品ジャンル一覧</h1>
+  <form action="db_genre.php" method="post">
+    <input type="text" size="50" name="genre_name">
+    <input type="submit" value="新規追加">
+  </form>
+  <table border=1>
+    <tr><th>ジャンルID</th><th>ジャンル名</th><th></th></tr>
+  <?php while($row=$sth->fetch(PDO::FETCH_ASSOC)){ ?>
+    <tr>
+      <td><?php ph($row["genre_id"]); ?></td>
+      <td><?php ph($row["genre_name"]); ?></td>
+      <td><a href="m_delete_exec_genre.php?genre_id=<?php ph($row["genre_id"]);?>">削除</a></td>
+    <tr>
+  <?php } ?>
+  </table>
+</body>
+</html>
