09db3e63ace42d0c78091be9dea1d965a4fd55c5
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-23 14:44:21 +0900

    Update m_delete_exec_material.php

--- m_control/m_delete_exec_material.php
+++ m_control/m_delete_exec_material.php
@@ -1,11 +1,12 @@
 <?php
 //制作者村上：2019.5.22：Ver1.0
 //美術品削除SQL発行プログラム（該当ID変数は$_GET["material_id"]）
+//更新者佐藤：2019.5.23
  ?>
 <?php
     require_once("../lib/init.php");
 
-    require_once
+    require_once("../lib/function/db_material.php");
+    $sth=delete_material($dbh);
 
     require_once("../lib/m_view/m_delete_exec_material.php");
-?>
