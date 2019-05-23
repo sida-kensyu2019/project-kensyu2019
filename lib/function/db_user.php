0f3433b2a776613896e4f0875820f52dda934504
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-23 10:47:06 +0900

    Create db_user.php

---
+++ lib/function/db_user.php
@@ -0,0 +1,72 @@
+<?php
+
+  function login_user()
+  {
+    session
+  }
+
+  //データベースに管理ユーザのデータを追加する
+  function m_insert_user()
+  {
+    try {
+        // プレースホルダ付きSQLを構築
+        $sql = "INSERT INTO m_user (mail_address, password, user_name, job_id, user_lv, profile) ";
+        $sql .= "VALUES (:mail_address, :password, :user_name, :job_id, 2, :profile)";
+        $sth = $dbh->prepare($sql); // SQLを準備
+
+        // プレースホルダに値をバインド
+        $sth->bindValue(":mail_address", $input["mail_address"]);
+        $sth->bindValue(":password", $input["password"]);
+        $sth->bindValue(":user_name", $input["user_name"]);
+        $sth->bindValue(":job_id", $input["job_id"]);
+        $sth->bindValue(":profile", $input["profile"]);
+
+        // SQLを発行
+        $sth->execute();
+
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
+    }
+        }
+
+    // データを戻す
+    return $sth;
+
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
+    }
+  }
+
+
+
+  //データベースに一般ユーザのデータを追加する
+  function m_insert_user()
+  {
+    try {
+        // プレースホルダ付きSQLを構築
+        $sql = "INSERT INTO m_user (mail_address, password, user_name, job_id, user_lv, profile) ";
+        $sql .= "VALUES (:mail_address, :password, :user_name, :job_id, 1, :profile)";
+        $sth = $dbh->prepare($sql); // SQLを準備
+
+        // プレースホルダに値をバインド
+        $sth->bindValue(":mail_address", $input["mail_address"]);
+        $sth->bindValue(":password", $input["password"]);
+        $sth->bindValue(":user_name", $input["user_name"]);
+        $sth->bindValue(":job_id", $input["job_id"]);
+        $sth->bindValue(":profile", $input["profile"]);
+
+        // SQLを発行
+        $sth->execute();
+
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
+    }
+        }
+
+    // データを戻す
+    return $sth;
+
+    } catch (PDOException $e) {
+        exit("SQL発行エラー：{$e->getMessage()}");
+    }
+  }
