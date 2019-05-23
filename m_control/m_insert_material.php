deaac554b21bec58237b25627250b8c727335b0b
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-23 14:42:10 +0900

    管理美術品登録画面コントローラ更新

--- m_control/m_insert_material.php
+++ m_control/m_insert_material.php
@@ -6,15 +6,31 @@ require_once("../init.php");
 //ユーザログイン確認
 if ($_SESSION["login"]) {
 // ログイン時
+
   // ユーザレベル判定
   if($_SESSION["user_lv"] == 2) { // 管理者のとき
     // 美術品登録画面ビュー出力
     require_once("../lib/m_view/m_insert_material.php");
+
+    // 内容漏れチェック
+    if (isset($_POST["mail_address"]) && isset($_POST["password"])
+     && isset($_POST["user_name"]) && isset($_POST["job_id"])) {
+
+       // 入力OK
+       require_once("../lib/m_view/m_insert_exec_material.php"); // 美術品登録完了画面出力
+     } else {
+
+       // 入力NG
+       require_once(".js"); // 入力エラー表示
+       require_once("../lib/m_view/m_insert_material.php"); // 美術品登録画面ビュー出力
+     }
   } else {  // 管理者でないとき
+
     // 美術館トップページビュー出力
     require_once("../lib/view/top.php");
   }
 } else { // ログインしていない場合
+
   // 美術館トップページビュー表示
   require_once("../lib/view/top.php");
 }
