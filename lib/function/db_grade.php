4b807fa263422e7d4cfae646e40226ab98d5d85a
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-23 16:02:54 +0900

    Update db_grade.php

--- lib/function/db_grade.php
+++ lib/function/db_grade.php
@@ -9,7 +9,7 @@
             $sql = "SELECT * FROM t_grade;";
             $sql .= "WHERE user_id=:user_id ";
             $sth = $dbh->prepare($sql); // SQLを準備
-    
+
             // プレースホルダに値をバインド
             $sth->bindValue(":user_id", $input["user_id"]);
 
@@ -18,7 +18,7 @@
 
             // データを戻す
             return $sth;
-            
+
         } catch (PDOException $e) {
             exit("SQL発行エラー：{$e->getMessage()}");
         }
@@ -33,7 +33,7 @@
             $sql = "SELECT * FROM t_grade;";
             $sql .= "WHERE material_id=:material_id ";
             $sth = $dbh->prepare($sql); // SQLを準備
-    
+
             // プレースホルダに値をバインド
             $sth->bindValue(":material_id", $input["material_id"]);
 
@@ -42,7 +42,7 @@
 
             // データを戻す
             return $sth;
-            
+
         } catch (PDOException $e) {
             exit("SQL発行エラー：{$e->getMessage()}");
         }
@@ -59,14 +59,14 @@
             $sql = "INSERT INTO t_grade (material_id, user_id, grade_date, star, comment) ";
             $sql .= "VALUES (:material_id, :user_id, :grade_date, :star, :comment)";
             $sth = $dbh->prepare($sql); // SQLを準備
-    
+
             // プレースホルダに値をバインド
             $sth->bindValue(":material_id", $input["material_id"]);
             $sth->bindValue(":user_id", $input["user_id"]);
             $sth->bindValue(":grade_date", $input["grade_date"]);
             $sth->bindValue(":star", $input["star"]);
             $sth->bindValue(":comment", $input["comment"]);
-    
+
             // SQLを発行
             $sth->execute();
 
@@ -84,10 +84,30 @@
             $sql = "DELETE FROM t_grade ";
             $sql .= "WHERE grade_id=:grade_id";
             $sth = $dbh->prepare($sql); // SQLを準備
-    
+
             // プレースホルダに値をバインド
             $sth->bindValue(":grade_id", (int) $id);
-    
+
+            // SQLを発行
+            $sth->execute();
+        } catch (PDOException $e) {
+            exit("SQL発行エラー：{$e->getMessage()}");
+        }
+    }
+
+    // データベースのデータを削除する
+    // $id: 削除するデータのid
+    function delete_grade_by_user($dbh, $id)
+    {
+        try {
+            // プレースホルダ付きSQLを構築
+            $sql = "DELETE FROM t_grade ";
+            $sql .= "WHERE user_id=:user_id";
+            $sth = $dbh->prepare($sql); // SQLを準備
+
+            // プレースホルダに値をバインド
+            $sth->bindValue(":user_id", (int) $id);
+
             // SQLを発行
             $sth->execute();
         } catch (PDOException $e) {
