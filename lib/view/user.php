d12115e94fefea8344dbbfba7dfc18f95cac0ae5
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-23 17:19:07 +0900

    Update user.php

--- lib/view/user.php
+++ lib/view/user.php
@@ -20,13 +20,13 @@
 <?php require_once("../init.php"); ?>
 
 <!-- ユーザ名表示 -->
-<h2><?php ph($POST_["user_name"]); ?></h2>
+<h2><?php ph($input["user_name"]); ?></h2>
 
 <!-- アイコン・評価コメント数・いいね数・自己紹介コメント -->
 <table border="1">
   <tr>
     <td>アイコン</td>
-    <td rowspan="3"><?php ph($POST_["comment"]); ?></td>
+    <td rowspan="3"><?php ph($input["comment"]); ?></td>
   </tr>
   <tr>
     <td>コメント数<br>
@@ -41,7 +41,7 @@
 過去の評価コメント<br>
 
 <!-- 管理者がアクセスした場合のみ表示 -->
-<a href="m_delete_exec_user.php">ユーザ削除</a><br>
+<input type="button" value="ユーザ削除" onclick="location.href='m_delete_exec_user.php'"><br>
 
 <!-- ユーザ本人がアクセスした場合のみ表示 -->
 <a href="delete_user.php">退会する</a>
