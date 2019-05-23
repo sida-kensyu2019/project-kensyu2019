3c197daa890738124ab5dc7b89e388171f086980
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-23 16:02:43 +0900

    Update m_insert_user.php

--- m_control/m_insert_user.php
+++ m_control/m_insert_user.php
@@ -1,26 +1,26 @@
 <?php
 // 制作者：井田　日時：2019.05.23 Ver1.0
 // 管理ユーザ登録画面コントローラ
-require_once("../init.php");
+require_once("../lib/init.php");
 
 //ユーザログイン確認
-if ($_SESSION["login"]) {
+//if ($_SESSION["login"]) {
 // ログイン時
 
   // ユーザレベル判定
-  if($_SESSION["user_lv"] == 2) { // 管理者のとき
+  //  if($_SESSION["user_lv"] == 2) { // 管理者のとき
 
     // 管理ユーザ登録画面ビュー出力
     require_once("../lib/m_view/m_insert_user.php");
 
-  } else {  // 管理者でないとき
+//  } else {  // 管理者でないとき
 
     // 美術館トップページビュー出力
-    require_once("../lib/view/top.php");
-  }
+//    require_once("../lib/view/top.php");
+  //}
 
-} else { // ログインしていない場合
-  
+//} else { // ログインしていない場合
+
   // 美術館トップページビュー表示
-  require_once("../lib/view/top.php");
-}
+//  require_once("../lib/view/top.php");
+//}
