968882a96b2406c5e58d0e66920f7c16af484a9f
Y-Masui <50697934+Y-Masui@users.noreply.github.com>
2019-05-23 14:21:13 +0900

    管理美術品編集画面コントローラ

---
+++ m_control/m_update_material.php
@@ -0,0 +1,14 @@
+<?php
+// 制作者:増井 2019/05/23：ver1.0
+// 管理美術品編集画面コントローラ
+
+	require_once("../lib/init.php");
+
+	//美術品テーブルの処理関数呼び出し
+	require_once("../lib/funtion/db_material.php");
+
+	//選択した美術品詳細データをデータベースから取得する
+	$sth = get_material_by_id($dbh, $_GET["id"]); //美術品ひとつを取得する関数
+
+	//美術品編集画面ビュー出力
+	require_once("../lib/m_view/m_update_material.php")
