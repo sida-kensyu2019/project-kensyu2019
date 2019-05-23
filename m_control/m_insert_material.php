cf4f1d9952c43c2e0b8c258bae47778f8133320b
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-23 14:27:01 +0900

    管理美術品登録画面コントローラ

--- m_control/m_insert_material.php
+++ m_control/m_insert_material.php
@@ -1,6 +1,6 @@
 <?php
 // 制作者：井田　日時：2019.05.23 Ver1.0
-// 管理ユーザ登録画面コントローラ
+// 管理美術品登録画面コントローラ
 require_once("../init.php");
 
 //ユーザログイン確認
@@ -8,8 +8,8 @@ if ($_SESSION["login"]) {
 // ログイン時
   // ユーザレベル判定
   if($_SESSION["user_lv"] == 2) { // 管理者のとき
-    // 管理ユーザ登録画面ビュー出力
-    require_once("../lib/m_view/m_insert_user.php");
+    // 美術品登録画面ビュー出力
+    require_once("../lib/m_view/m_insert_material.php");
   } else {  // 管理者でないとき
     // 美術館トップページビュー出力
     require_once("../lib/view/top.php");
