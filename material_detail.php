9de09f7259a1f2d3276dd07fa1eb620aa9ab1e9a
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-23 11:54:41 +0900

    美術品詳細画面コントローラ

---
+++ material_detail.php
@@ -0,0 +1,13 @@
+<?php
+// 制作者：井田　日時：2019.05.23 Ver1.0
+// 美術品詳細画面コントローラ
+requie_once("../init.php");
+
+  // 美術品テーブルの処理関数呼び出し
+  require_once("/lib/function/db_material.php");
+
+  // 美術品詳細データをデータベースから取得
+  $sth = get_material_by_id($dbh, $_GET["material_id"]);
+
+  // 美術品詳細画面ビュー出力
+  require_once("/view/material_detail.php");
