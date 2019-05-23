ee376eb24df3c15193ca50c8e75014491af8112c
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-23 17:28:41 +0900

    ログイン判断コメントアウト、管理美術品登録画面コントローラ更新

--- m_control/m_insert_material.php
+++ m_control/m_insert_material.php
@@ -1,24 +1,26 @@
 <?php
 // 制作者：井田　日時：2019.05.23 Ver1.0
 // 管理美術品登録画面コントローラ
-require_once("../init.php");
+require_once("../lib/init.php");
 
 //ユーザログイン確認
-if ($_SESSION["login"]) {
+// if ($_SESSION["login"]) {
 // ログイン時
 
   // ユーザレベル判定
-  if($_SESSION["user_lv"] == 2) { // 管理者のとき
+  // if($_SESSION["user_lv"] == 2) { // 管理者のとき
 
    if (empty($_POST)) {
     // 美術品登録画面ビュー出力
+    require_once("../lib/function/db_material.php");
+    $sth=get_material($dbh);
     require_once("../lib/m_view/m_insert_material.php");
    } else {
     // 内容漏れチェック
     if (isset($_POST["material_name"]) && isset($_POST["material_kana"])
      && isset($_POST["author_name"]) && isset($_POST["author_kana"])
      && isset($_POST["genre_id"]) && isset($_POST["material_year"])
-     && && isset($_POST["caption"])) {
+     && isset($_POST["caption"])) {
 
        // 入力OK
        require_once("../lib/m_view/m_insert_exec_material.php"); // 美術品登録完了画面出力
@@ -29,13 +31,13 @@ if ($_SESSION["login"]) {
        require_once("../lib/m_view/m_insert_material.php"); // 美術品登録画面ビュー出力
      }
    }
-  } else {  // 管理者でないとき
-
-    // 美術館トップページビュー出力
-    require_once("../lib/view/top.php");
-  }
-} else { // ログインしていない場合
-
-  // 美術館トップページビュー表示
-  require_once("../lib/view/top.php");
-}
+//   } else {  // 管理者でないとき
+//
+//     // 美術館トップページビュー出力
+//     require_once("../lib/view/top.php");
+//   }
+// } else { // ログインしていない場合
+//
+//   // 美術館トップページビュー表示
+//   require_once("../lib/view/top.php");
+// }
