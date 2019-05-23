7079c782b66cf2ee495802d3da7d32cf212f957d
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-23 16:59:21 +0900

    管理コントローラ更新

--- m_control/m_insert_user.php
+++ m_control/m_insert_user.php
@@ -10,15 +10,30 @@ require_once("../lib/init.php");
   // ユーザレベル判定
   //  if($_SESSION["user_lv"] == 2) { // 管理者のとき
 
-    require_once("../lib/function/db_job.php");
-    $sth=get_job($dbh);
+    // 最初にアクセスした場合
+    if (empty($_POST)) {
+      // 管理ユーザ登録画面ビュー出力
+      require_once("../lib/function/db_job.php");
+      $sth=get_job($dbh);
+      require_once("../lib/m_view/m_insert_user.php");
+    } else {
+      // 内容漏れチェック
+      if (empty($_POST["mail_address"]) || empty($_POST["password"])
+        || empty($_POST["user_name"]) || empty($_POST["job_id"])) {
+        // require_once(".js"); // 入力エラー表示、JavaScriptがないためコメントアウト
+        require_once("../lib/m_view/m_insert_user.php");
+      } else {
 
-    // 管理ユーザ登録画面ビュー出力
-    require_once("../lib/m_view/m_insert_user.php");
+        // 入力OK
+        //データベースに管理ユーザを追加
+        require_once("../lib/function/db_user.php");
+        $sth=insert_m_user($dbh, $input);
+        require_once("../lib/m_view/m_insert_exec_user.php");
+      }
+    }
 
-    //データベースに管理ユーザを追加
-    require_once("../lib/function/db_user.php");
-    $sth=insert_m_user($dbh, $input);
+
+
 
 //  } else {  // 管理者でないとき
 
