3ebe7484eb5095deca80a15ab2a4009509b22017
Y-Masui <50697934+Y-Masui@users.noreply.github.com>
2019-05-23 15:06:13 +0900

    Update m_select_material.php

--- m_control/m_select_material.php
+++ m_control/m_select_material.php
@@ -5,10 +5,10 @@
 	require_once("../lib/init.php");
 
 	//美術品テーブルの処理関数呼び出し
-	require_once("../lib/funtion/db_material.php");
+	require_once("../lib/function/db_material.php");
 
 	//美術品データをデータベースから取得する
 	$sth = get_material($dbh); //美術品すべてを取得する関数
 
 	//美術品一覧画面ビュー出力
-	require_once("../lib/m_view/m_select_material.php")
+	require_once("../lib/m_view/m_select_material.php");
