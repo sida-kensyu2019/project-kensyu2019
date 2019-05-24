42dc00aa64511eee2a6ab7eb1a8a620ac05c7c91
YiwaiY <45761076+YiwaiY@users.noreply.github.com>
2019-05-24 18:41:37 +0900

    ヘッダ

--- lib/view/update_user.php
+++ lib/view/update_user.php
@@ -13,9 +13,25 @@
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="stylesheet" href="css/style.css">
 <title>ユーザ情報変更画面</title>
 </head>
 <body>
+<header>
+  <nav>
+    <h1>愛パワー美術館</h1>
+    <ul>
+      <li><a href="top.php">トップページ</a></li>
+      <?php if (user_lv_check()) { ?>
+        <li><a href="logout.php" onclick="return confirm('本当にログアウトしますか？');">ログアウト</a></li>
+        <li><a href="usr.php?user_id=<?php ph($_SESSION["user_id"]); ?>">マイページ</a></li>
+      <?php } else { ?>
+      <li><a href="login.php">ログイン</a></li>
+      <li><a href="insert_user.php">新規登録</a></li>
+      <?php } ?>
+    </ul>
+  </nav>
+</header>
 <h2>ユーザ情報変更画面</h2>
 
 <table>
