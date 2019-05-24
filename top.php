816ceff9fa2e4303b1c4f980ee16996a905497ad
7mura <50697386+7mura@users.noreply.github.com>
2019-05-24 09:33:09 +0900

    一部修正TOP

--- top.php
+++ top.php
@@ -9,11 +9,11 @@ require_once("lib/init.php");
 
 //カレンダーSQL召喚
 require_once("lib/function/db_closed.php");
-  get_closed($dbh);
+  $sth_closed = get_closed($dbh);
 require_once("lib/function/calender_function.php")
   mkCalender();
   //休館日データをすべて二次元配列に格納
-  $closed_list = $sth->fetchAll(PDO::FETCH_ASSOC);
+  $closed_list = $sth_closed->fetchAll(PDO::FETCH_ASSOC);
 
 //検索ボックス用関数召喚
 require_once("lib/function/db_material.php");
@@ -21,7 +21,7 @@ require_once("lib/function/db_material.php");
 
 //表示する美術品召喚SQL
 require_once("lib/function/db_material.php");
- $sth = get_material_top($dbh);
+ $rowTop = get_material_top($dbh);
 
 //view呼び込み
 require_once("lib/view/top.php");
