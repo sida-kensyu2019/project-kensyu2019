293fe4a6555cc2d11d01b55d6df3e6c3b3fbfbe8
Y-Masui <50697934+Y-Masui@users.noreply.github.com>
2019-05-24 11:33:30 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,63 +1,37 @@
-/**
- * このファイルの概要説明
- * ログイン画面
- * このファイルの詳細説明
- * 非ログインユーザが、管理者、ログインユーザとしての操作権限を得るためにログインする機能
- * システム名：愛パワー美術品評価管理システム
- * 作成者：山田美波
- * 作成日：2019/05/22
- * 最終更新日：2019/05/22
- * レビュー担当者：
- * レビュー日：
- * バージョン：0.1
- */
+<?php
+	//制作者：増井 日時：2019/05/23-24 ver1.0
+	//ログイン画面コントローラ
 
-<?php require_once("lib/init.php"); ?>
+	require_once("lib/init.php");
+	require_once("lib/function/function.php");
+	require_once("lib/function/db_user.php");
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>ログイン画面</title>
-</head>
-<body>
-  //あとで、一画面遷移ができるように
-  <form action="login.php" method="post">
+	if(empty($_POST)){ //初回アクセス時
+		$_SESSION["login"]="";//$_SESSIONの初期化
+		$errorMessage = ""; //エラーメッセージの初期化
+		require_once("lib/view/login.php"); //ログイン画面を表示
+	}else if($_SESSION["login"]==true){ //既にログインしたユーザがアクセスした時
+ 		require_once("index.php"); //トップ画面に遷移
+ 	}else{
+		if(empty($_POST["mail_address"] || $_POST["password"])){ //入力されてない時
+			$errorMessage="メールアドレスまたはパスワードが未入力です";
+			require_once("lib/view/login.php"); //ログイン画面をもう一度表示
+		}else{ //入力された時データベースに問い合わせる
+			$sth=get_user_by_mail($dbh, $_POST);
+			$row=$sth->fetch(PDO::FETCH_ASSOC); //入力されたメールアドレスのユーザデータを配列にして取得
+			if(empty($row)){ //メールアドレスがデータベースに無い時
+				$errorMessage="メールアドレスまたはパスワードが違います";
+				require_once("lib/view/login.php"); //ログイン画面をもう一度表示
+			}else if($_POST["password"]===$row["password"]){ //メールアドレスとパスワードが正しかった時
+				$_SESSION["user_id"]=$row["user_id"]; //$_SESSIONにユーザIDを持たせる
+				$_SESSION["user_lv"]=$row["user_lv"]; //$_SESSIONにユーザLVを持たせる
+				$_SESSION["login"]=true; //$_SESSIONのログイン状態を切り替え
+				require_once("index.php"); //トップ画面に遷移
+			}else{
+				$errorMessage="メールアドレスまたはパスワードが違います";
+				require_once("lib/view/login.php"); //ログイン画面をもう一度表示
+			}
+		}
+	}
 
-    ID:<input type="text" name="user_id" value="<?php
-    if(!empty($_POST["user_id"])){
-      ph($_POST["user_id"]);
-    } else {
-      print "";
-    }
-     ?>"><br>
-    パスワード:<input type="password" name="password"  value="<?php
-    if(!empty($_POST["password"])){
-      ph($_POST["password"]);
-    } else {
-      print "";
-    }
-     ?>"><br>
-    <input type="submit" value="ログイン"><br>
-
-  </form>
-
-  <?php
-    if(empty($_POST)){
-
-    } else {
-      //データベースに問い合わせる
-      if($_POST["user_id"]=="user_id" and $_POST["password"]=="password"){
-        //ログインをして、美術館トップ画面に遷移
-        $_SESSION["login"]=true;
-        header("top.php");
-      } else {
-        print "IDまたはパスワードが違います";
-        $_SESSION["login"]=false;
-      }
-    }
-
-  ?>
-
-</body>
-</html>
+	?>
