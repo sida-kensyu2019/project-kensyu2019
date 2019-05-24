f693d6c12b07555961ea7664e32fe20e6713b372
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-24 13:38:02 +0900

    Update m_select_job.php

--- lib/m_view/m_select_job.php
+++ lib/m_view/m_select_job.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>職種一覧画面</title>
-    <link rel="stylesheet" href="../../css/m_style.css">
+    <link rel="stylesheet" href="../css/m_style.css">
   </head>
   <body>
     <header>
