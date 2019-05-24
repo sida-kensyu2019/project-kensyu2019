e4709e314a4d5431725f929207014807dfd562f3
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-24 13:27:23 +0900

    Update header.php

--- lib/m_view/header.php
+++ lib/m_view/header.php
@@ -4,21 +4,22 @@
      <head>
        <meta charset="UTF-8">
        <title>ヘッダー</title>
-       <link rel="stylesheet" href="../css/m_style.css">
+       <link rel="stylesheet" href="../../css/m_style.css">
        <script src=""></script>
      </head>
      <body>
+
        <header>
-         <h1>管理画面</h1>
          <nav>
+           <h1>管理者画面</h1>
            <ul>
-             <li><a href="m_top.php">トップページ</a></li>
-             <li><a href="../../m_control/m_select_material.php">美術品一覧画面</a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
-             <li><a href=""></a></li>
+             <li><a href="m_top.html">トップページ</a></li>
+             <li><a href="../../m_control/m_select_material.php">美術品一覧</a></li>
+             <li><a href="../../m_control/m_select_user.php">管理者一覧</a></li>
+             <li><a href="../../m_control/m_select_genre.php">ジャンル一覧</a></li>
+             <li><a href="../../m_control/m_select_job.php">職種一覧</a></li>
+             <li><a href="../../m_control/m_select_closed.php">休館日一覧</a></li>
+             <li><a href="">ログアウト</a></div>
            </ul>
          </nav>
        </header>
