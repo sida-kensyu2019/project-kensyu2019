5ffe35b8a18d6e310c1478720b90edeb420fd8f6
7mura <50697386+7mura@users.noreply.github.com>
2019-05-24 11:04:17 +0900

    Update select_material.php

--- select_material.php
+++ select_material.php
@@ -8,7 +8,7 @@
 	require_once("lib/function/db_material.php");
 
 	//美術品データをデータベースから取得する
-	$sth = get_material($dbh); //美術品すべてを取得する関数
+	$sth = get_material($dbh,$_POST); //美術品すべてを取得する関数
 
 	//美術品一覧画面ビュー出力
 	require_once("lib/view/select_material.php");
