7f8802997d1f2835c518a6baa5fd5a419cde903c
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-27 16:42:29 +0900

    Update insert_exec_good.php

--- insert_exec_good.php
+++ insert_exec_good.php
@@ -6,17 +6,25 @@
 
   $sth=get_good_by_user($dbh, $_POST["user_id"]);
 
-  $no_good = true;
+  //$no_good = true;
+
+  // while($row=$sth->fetch(PDO::FETCH_ASSOC)){
+  //   if($row["grade_id"] == $_POST["grade_id"]){
+  //     delete_good($dbh, $_POST);
+  //     $no_good = false;
+  //   }
+  // }
+  //
+  // if ($no_good){
+  //   insert_good($dbh, $_POST);
+  // }
 
   while($row=$sth->fetch(PDO::FETCH_ASSOC)){
     if($row["grade_id"] == $_POST["grade_id"]){
       delete_good($dbh, $_POST);
-      $no_good = false;
-    }
-  }
-
-  if ($no_good){
+      } else {
     insert_good($dbh, $_POST);
+      }
   }
 
   header("Location:material_detail.php?material_id={$_POST["material_id"]}");
