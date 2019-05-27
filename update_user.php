349b468d057d1657d439c58edad29ea2f125a58a
sida-kensyu2019 <50697427+sida-kensyu2019@users.noreply.github.com>
2019-05-27 09:58:09 +0900

    ユーザ情報変更画面まわりを修正、新規作成

---
+++ update_user.php
@@ -0,0 +1,40 @@
+<?php
+// 制作者：井田　日時：2019.05.27 Ver1.0
+// ユーザ情報変更画面
+require_once("lib/init.php");
+
+// ユーザIDとユーザページのIDがが同じかどうか
+var_dump($_GET["user_id"]);
+var_dump($_SESSION["user_id"]);
+if ($_GET["user_id"] == $_SESSION["user_id"]) {
+
+  // ユーザIDとユーザページのIDが一致
+  // 最初にアクセスした場合
+  if(empty($_POST)) {
+
+    // ユーザ情報変更画面ビュー出力
+    require_once("lib/function/db_job.php");
+    $sth = get_job($dbh);
+    $msg = "";
+    require_once("lib/view/update_user.php");
+
+  } else {
+
+    // 内容漏れチェック
+    if (empty($_POST["password"]) || empty($_POST["user_name"]) || empty($_POST["job_id"])) {
+
+      //入力NG
+      require_once("lib/function/db_job.php");
+      $sth = get_job($dbh);
+      $msg = "すべて入力してください";
+      require_once("lib/view/update_user.php");
+
+    } else {
+
+      //入力OK
+      require_once("lib/function/db_job.php");
+      $sth=get_job($dbh);
+      require_once("update_user.php");
+    }
+  }
+}
