1d21f98cfedc126523489574b1dd4d2f9a20546f
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-27 16:04:46 +0900

    Update material_detail.php

--- material_detail.php
+++ material_detail.php
@@ -11,7 +11,6 @@
   // 美術品詳細データをデータベースから取得
   $sth_material = get_material_by_id($dbh, $_GET["material_id"]);
 
-
   //該当美術品IDの評価件数を取得
   require_once("lib/function/db_grade.php");
   $count = grade_count($dbh, $_GET["material_id"]);
@@ -19,16 +18,5 @@
   // 評価詳細一覧表示取得
   $sth_grade = get_grade_by_material($dbh, $_GET["material_id"]);
 
-
-
   // 美術品詳細画面ビュー出力
   require_once("lib/view/material_detail.php");
-
-  require_once("lib/function/db_good.php");
-
-  $_POST["user_id"] = $_SESSION["user_id"];
-
-  if (isset($_POST["grade_id"])){
-	   $_POST["grade_id"] = $row2["user_id"];
-     $sth_insert_grade = insert_good($dbh, $_POST);
-   }
