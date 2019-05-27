d5422978ab47d21d7119a73d2f7dda2420f676da
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-27 19:53:17 +0900

    Update update_user.php

--- lib/view/update_user.php
+++ lib/view/update_user.php
@@ -21,13 +21,13 @@
   <nav>
     <h1>愛パワー美術館</h1>
     <ul>
-      <li><a href="top.php">トップページ</a></li>
+      <li><a href="top.php" class="li_a">トップページ</a></li>
       <?php if (login_check()) { ?>
-        <li><a href="logout.php" onclick="return confirm('本当にログアウトしますか？');">ログアウト</a></li>
-        <li><a href="user.php?user_id=<?php ph($_SESSION["user_id"]); ?>">マイページ</a></li>
+        <li><a href="logout.php" onclick="return confirm('本当にログアウトしますか？');" class="li_a">ログアウト</a></li>
+        <li><a href="user.php?user_id=<?php ph($_SESSION["user_id"]); ?>" class="li_a">マイページ</a></li>
       <?php } else { ?>
-      <li><a href="login.php">ログイン</a></li>
-      <li><a href="insert_user.php">新規登録</a></li>
+      <li><a href="login.php" class="li_a">ログイン</a></li>
+      <li><a href="insert_user.php" class="li_a">新規登録</a></li>
       <?php } ?>
     </ul>
   </nav>
