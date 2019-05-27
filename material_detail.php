5897b4c5144f87d748990e40002b50c34a941c84
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-27 14:17:15 +0900

    Update material_detail.php

--- material_detail.php
+++ material_detail.php
@@ -19,6 +19,9 @@
   // 評価詳細一覧表示取得
   $sth_grade = get_grade_by_material($dbh, $_GET["material_id"]);
 
-
   // 美術品詳細画面ビュー出力
   require_once("lib/view/material_detail.php");
+
+  if(isset($_POST["good"])) {
+      $sth_insert_grade = insert_good($dbh, $_POST); //いいねの追加
+  } 
