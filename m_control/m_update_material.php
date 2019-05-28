f69e8daa93cb33e101b58890045935465fd10666
SatoRuna <50697864+SatoRuna@users.noreply.github.com>
2019-05-28 10:48:31 +0900

    Update m_update_material.php

--- m_control/m_update_material.php
+++ m_control/m_update_material.php
@@ -32,10 +32,10 @@
      || empty($_POST["author_name"]) || empty($_POST["author_kana"])
      || empty($_POST["genre_id"]) || empty($_POST["material_year"])
      || empty($_POST["picture"]) || empty($_POST["caption"])
-     || !preg_match("/^[^\x01-\x7E]{1,50}$/", $_POST["material_name"])
-     || !preg_match("/^[ぁ-ゞ・＝]{1,70}$/u", $_POST["material_kana"])
-     || !preg_match("/^[^\x01-\x7E]{1,50}$/", $_POST["author_name"])
-     || !preg_match("/^[ぁ-ゞ・＝]{1,70}$/u", $_POST["author_kana"])
+     || mb_strlen($_POST["material_name"]) > 50
+     || !preg_match("/^[ぁ-ゞ・＝゛]{1,70}$/u", $_POST["material_kana"])
+     || mb_strlen($_POST["author_name"]) > 50
+     || !preg_match("/^[ぁ-ゞ・＝゛]{1,70}$/u", $_POST["author_kana"])
      || mb_strlen($_POST["material_year"]) > 20 || mb_strlen($_POST["picture"]) > 100
      || mb_strlen($_POST["caption"]) > 200) {
 			// require_once(".js"); // 入力エラー表示、JavaScriptがないためコメントアウト
