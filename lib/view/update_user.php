43bde65192f866ebcd7f8e702a152d6fd32dc04b
7mura <50697386+7mura@users.noreply.github.com>
2019-05-28 13:37:12 +0900

    ユーザ情報変更画面CSS

--- lib/view/update_user.php
+++ lib/view/update_user.php
@@ -20,7 +20,7 @@
   <div class="back">
 <header>
   <nav>
-    <h1>愛パワー美術館</h1>
+    <img src="image/titlelogo.png" height="90px">
     <ul>
       <li><a href="top.php" class="li_a">トップページ</a></li>
       <?php if (login_check()) { ?>
@@ -34,16 +34,14 @@
   </nav>
 </header>
 <h2>ユーザ情報変更画面</h2>
-
-<table>
-  <form action="update_exec_user.php" method="post">
-
+<form action="update_exec_user.php" method="post" id="form_update">
+  <table id="table_search">
     <tr><th>メールアドレス</th><td><?php ph($sth["mail_address"]) ?></td></tr>
-    <tr><th>パスワード</th><td><input type="password" size="30" name ="password"></td></tr>
-    <tr><th>表示名</th><td><input type="text" size="30" name ="user_name"></td></tr>
+    <tr><th>パスワード</th><td><input type="password" size="30" name ="password" class="update_user"></td></tr>
+    <tr><th>表示名</th><td><input type="text" size="30" name ="user_name" class="update_user"></td></tr>
     <tr><th>職業</th>
       <td>
-      <select name="job_id">
+      <select name="job_id" class="update_user2">
         <?php while ($row = $sth_job->fetch(PDO::FETCH_ASSOC)) { ?>
           <option value="<?php ph($row["job_id"]); ?>"><?php ph($row["job_name"]) ?></option>
         <?php } ?>
@@ -53,18 +51,20 @@
     <tr>
       <th>プロフィールコメント</th>
       <td>
-        <textarea cols="40" rows="5" name="profile">よろしくお願いします。</textarea>
+        <textarea cols="32" rows="5" name="profile" class="update_user3">よろしくお願いします。</textarea>
       </td>
     </tr>
     <input type="hidden" name="user_id" value="<?php ph($_SESSION["user_id"]); ?>">
-    <tr><th>
-    <input type="submit" name="変更">
-    <input type="reset" name="クリア">
-  </tr></th>
-    <br>
-  </form>
-</table>
+  </table>
+  <div class="right">
+    <input type="submit" value="変更">
+    <input type="reset" value="クリア">
+  </div>
+</form>
+<br>
 <a href="user.php?user_id=<?php ph($_SESSION["user_id"]); ?>">マイページに戻る</a>
+<br>
+<br>
 </div>
 </body>
 </html>
