f204a749216136c7f3c0c1b1847f6cf06656e1d1
YiwaiY <45761076+YiwaiY@users.noreply.github.com>
2019-05-29 16:03:14 +0900

    init.phpを改造しました。バグ生まれたらすみません。

--- lib/init.php
+++ lib/init.php
@@ -27,6 +27,17 @@
     header('Cache-Control:');
     header('Pragma:');
 
+    //セッションに保存されたユーザがデータベースから消えているとき、セッションを切る
+    if (!empty($_SESSION["user_id"])) {
+        require_once("function/db_user.php");
+        $row_user_id = get_user_by_id($dbh, $_SESSION["user_id"]);
+        if (empty($row_user_id["user_id"])) {
+            $_SESSION["user_id"]=false; //$_SESSIONにユーザIDを持たせる
+            $_SESSION["user_lv"]=false; //$_SESSIONにユーザLVを持たせる
+            $_SESSION["login"]=false; //$_SESSIONのログイン状態を切り替え
+        }
+    }
+
     //文字コード設定
     mb_internal_encoding("UTF-8");
 
