b64ab224e37b25ed705987d4ccede9573ae5f144
YiwaiY <45761076+YiwaiY@users.noreply.github.com>
2019-05-29 13:43:43 +0900

    ジャンル表示

--- lib/m_view/m_update_exec_material.php
+++ lib/m_view/m_update_exec_material.php
@@ -29,7 +29,14 @@
       <tr><th>美術品名読み</th><th><?php ph($_POST["material_kana"]) ?></th></tr>
       <tr><th>作者名</th><th><?php ph($_POST["author_name"]) ?></th></tr>
       <tr><th>作者名読み</th><th><?php ph($_POST["author_kana"]) ?></th></tr>
-      <tr><th>ジャンル</th><th><?php ph($_POST["genre_id"]) ?></th></tr>
+      <tr><th>ジャンル</th>
+      <th>
+        <?php while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+          if ($row["genre_id"] == $_POST["genre_id"]) {
+            ph($row["genre_name"]);
+          }
+        }?>
+      </th></tr>
       <tr><th>制作年</th><th><?php ph($_POST["material_year"]) ?></th></tr>
       <tr><th>写真</th><th><?php ph($_POST["picture"]) ?></th></tr>
       <tr><th>説明</th><th><?php ph($_POST["caption"]) ?></th></tr>
